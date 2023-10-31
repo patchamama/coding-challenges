4a5251018db9d4769229924f8ef5315941df033c
A. Urquiola <patchamama.go@gmail.com>
2023-10-31 11:39:18 +0100

    add typescript solution: resistor-color-duo

---
+++ exercism/typescript/resistor-color-duo/resistor-color-duo.ts
@@ -0,0 +1,16 @@
+const colorMap: { [key: string]: number } = {
+  black: 0,
+  brown: 1,
+  red: 2,
+  orange: 3,
+  yellow: 4,
+  green: 5,
+  blue: 6,
+  violet: 7,
+  grey: 8,
+  white: 9,
+};
+
+export function decodedValue(colors: string[]): number {
+  return colorMap[colors[0]]*10 + colorMap[colors[1]];
+}
