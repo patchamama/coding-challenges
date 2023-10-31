27b658e75c54f42f529a01954c49d823202d291d
A. Urquiola <patchamama.go@gmail.com>
2023-10-31 04:18:53 +0100

    add typescript solution: raindrops

---
+++ exercism/typescript/raindrops/raindrops.ts
@@ -0,0 +1,16 @@
+
+export const convert = (number: number): string => {
+  let result = "";
+
+  if (number % 3 === 0) {
+    result += "Pling";
+  } 
+  if (number % 5 === 0) {
+    result += "Plang";
+  } 
+  if (number % 7 === 0) {
+    result += "Plong";
+  } 
+
+  return result === "" ? number.toString() : result;
+};
