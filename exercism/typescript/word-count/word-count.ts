3f2a558790ffbeedfb15376e9a8106fe02144678
A. Urquiola <patchamama.go@gmail.com>
2023-11-01 10:46:35 +0100

    add typescript solution: word-count

--- exercism/typescript/word-count/word-count.ts
+++ exercism/typescript/word-count/word-count.ts
@@ -1,3 +1,3 @@
 export function count(phrase: string) {
- return new Map(Object.entries((' '+phrase+' ').toLowerCase().replace(/[^a-z\d\']/g, ' ').replace('constructor','konstructor').replace(/ \'|\' /g, ' ').replace(/\s+/g, ' ').trim().split(' ').sort().reduce( (counter: { [key: string]: number }, word )  => {counter[word.replace('konstructor','constructor')] = (counter[word] || 0) + 1; return counter; }, {})))
+ return new Map(Object.entries( [...(phrase.toLowerCase().replace('constructor','konstructor').match(/\b[\w']+\b/g) ?? [])].reduce( (counter: { [key: string]: number }, word )  => {counter[word.replace('konstructor','constructor')] = (counter[word] || 0) + 1; return counter; }, {})))
 }
